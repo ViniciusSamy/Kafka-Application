4b2dac3269bd5ddbb6aa19f233c309b88190387b
ViniciusSamy <vinicius_samy@outlook.com>
2021-12-07 00:25:52 -0300

    Adicionando Scripts de conf das VMs (corrigido 3)

--- BashCommands/consumer.sh
+++ BashCommands/consumer.sh
@@ -5,4 +5,5 @@ sudo pip3 install kafka-python python-dateutil
 sudo wget https://dlcdn.apache.org/kafka/3.0.0/kafka_2.13-3.0.0.tgz
 #sudo git clone https://github.com/ViniciusSamy/SD
 sudo tar -zxvf kafka_2.13-3.0.0.tgz
+sudo rm kafka_2.13-3.0.0.tgz
 
